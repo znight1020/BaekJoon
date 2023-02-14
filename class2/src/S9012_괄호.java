013eb0b5ed7a41b95a0b915f1b7891574e04b57f
현수 <znight1020@naver.com>
2023-02-14 15:07:28 +0900

    S9012_괄호

--- class2/src/S9012_괄호.java
+++ class2/src/S9012_괄호.java
@@ -8,63 +8,47 @@ public class S9012_괄호 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        String str, sub;
         Stack<Character> arr;
-        int top;
+        String str;
+        int top, cnt = 0;
         boolean flag;
 
-        while((str = br.readLine()) != null){ // 문자열 단위
+        int size = Integer.parseInt(br.readLine());
+
+        while(cnt < size){ // 문자열 단위
             flag = true;
             top = 0;
-            if(str.equals(".")){
-                break;
-            }
             arr = new Stack<>();
-            st = new StringTokenizer(str);
 
-            while(st.hasMoreTokens()){ // 단어 단위
-                sub = st.nextToken();
-                for(int i = 0; i < sub.length(); i++){ // 글자 단위
-                    if(top == 0 && sub.charAt(i) == ')'){
+            str = br.readLine();
+
+            for(int i = 0; i < str.length(); i++){ // 글자 단위
+                if(top == 0 && str.charAt(i) == ')'){
+                    flag = false;
+                    break;
+                }
+
+                if(str.charAt(i) == '('){
+                    arr.push(str.charAt(i));
+                    top++;
+                }
+                else if(str.charAt(i) == ')' && top > 0){
+                    if(arr.peek() == '(') {
+                        arr.pop();
+                        top--;
+                    }
+                    else {
                         flag = false;
                         break;
-                    }
-                    else if(top == 0 && sub.charAt(i) == ']'){
-                        flag = false;
-                        break;
-                    }
-
-                    if(sub.charAt(i) == '(' || sub.charAt(i) == '['){
-                        arr.push(sub.charAt(i));
-                        top++;
-                    }
-                    else if(sub.charAt(i) == ')' && top > 0){
-                        if(arr.peek() == '(') {
-                            arr.pop();
-                            top--;
-                        }
-                        else {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    else if(sub.charAt(i) == ']' && top > 0){
-                        if(arr.peek() == '[') {
-                            arr.pop();
-                            top--;
-                        }
-                        else {
-                            flag = false;
-                            break;
-                        }
                     }
                 }
-                if(sub.contains(".")) break; // 온점이 찍힌 경우 해당 문자열 끝
             }
             if(!arr.isEmpty()) flag = false;
 
-            if(flag == true) System.out.println("yes");
-            else System.out.println("no");
+            if(flag == true) System.out.println("YES");
+            else System.out.println("NO");
+
+            cnt++;
         }
     }
 }
