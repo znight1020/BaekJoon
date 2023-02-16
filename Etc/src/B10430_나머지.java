56415e7cf16ed6c5f3ec256b638792eb867e618f
현수 <znight1020@naver.com>
2023-02-16 21:09:28 +0900

    B10430_나머지

---
+++ Etc/src/B10430_나머지.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class B10430_나머지 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int C = scan.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+    }
+}
